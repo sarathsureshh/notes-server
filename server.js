710b1e5b3202ab37d3c5eabaf95295a056f01fcb
Sarath Suresh <sarathsuresh9360@gmail.com>
2024-09-24 21:59:04 +0530

    Add morgan and refactor server.js

--- server.js
+++ server.js
@@ -1,38 +1,68 @@
-import bodyParser from "body-parser";
 import express, { Router } from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
 import dotenv from "dotenv";
 dotenv.config();
-
-const envPort = process.env.PORT || 3000;
 
 import { initiateDb } from "./src/utils/mongoUtils.js";
 import { notesServices } from "./src/services/notesServices.js";
 import { authServices } from "./src/services/authService.js";
 
-const { json } = bodyParser;
 const app = express();
-var routePath = Router();
-app.use(json());
+const routePath = Router();
+const envPort = process.env.PORT || 3000;
 
-//Initiate all the services
+app.use(bodyParser.json());
+
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      `Status: ${tokens.status(req, res)}`,
+      `Response time: ${tokens["response-time"](req, res)} ms`,
+      `Client IP: ${tokens["remote-addr"](req, res)}`,
+      `User-agent: ${tokens["user-agent"](req, res)}`,
+      `Content length: ${tokens.res(req, res, "content-length") || 0} bytes`,
+    ].join(" | ");
+  })
+);
+
 authServices(routePath);
 notesServices(routePath);
 
-//Appends /services to all the routes that come in
 app.use("/services", routePath);
 
-initiateDb(function (err) {
-  if (err) {
-    console.error(err.stack);
-    return;
-  }
-  var server = app.listen(envPort, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log(`Server listening at http://${host}:${port}`);
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({
+    message: "An internal server error occurred.",
+    error: err.message,
   });
 });
 
-process.on("uncaughtException", function (err) {
-  console.error(err.stack);
+const startServer = async () => {
+  try {
+    await new Promise((resolve, reject) => {
+      initiateDb((err) => {
+        if (err) return reject(err);
+        resolve();
+      });
+    });
+
+    const server = app.listen(envPort, () => {
+      const host = server.address().address;
+      const port = server.address().port;
+      console.log(`Server listening at http://${host}:${port}`);
+    });
+  } catch (err) {
+    console.error("Failed to initiate DB or start server:", err.stack);
+    process.exit(1);
+  }
+};
+
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught exception:", err.stack);
 });
+
+startServer();
